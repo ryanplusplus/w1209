4eefdffc2e7ce71004d77d2510a37e9ab95696a1
Ryan Hartlage <ryanplusplus@gmail.com>
2020-04-12 12:54:48 -0400

    Cleaned up

--- src/device/display.c
+++ src/device/display.c
@@ -5,228 +5,170 @@
 
 #include <stddef.h>
 #include <string.h>
+#include <stdbool.h>
 #include "stm8s_gpio.h"
 #include "display.h"
+#include "tiny_utils.h"
 
-#define init_output(_port, _pin)   \
-  GPIO##_port->DDR |= (1 << _pin); \
-  GPIO##_port->CR1 |= (1 << _pin)
+enum {
+  digit_on = 0,
+  digit_off = 1
+};
 
-#define write_output(_port, _pin, _state) \
-  if(_state)                              \
-    GPIO##_port->ODR |= (1 << _pin);      \
-  else                                    \
-    GPIO##_port->ODR &= ~(1 << _pin)
+#define seg_none 0x00
+#define seg_a 0x01
+#define seg_b 0x02
+#define seg_c 0x04
+#define seg_d 0x08
+#define seg_e 0x10
+#define seg_f 0x20
+#define seg_g 0x40
+#define seg_p 0x80
+
+#define output(_port, _pin) \
+  {                         \
+    GPIO##_port,            \
+      (1 << _pin)           \
+  }
+
+typedef struct {
+  GPIO_TypeDef* port;
+  uint8_t mask;
+} output_t;
+
+static const output_t digit[] = {
+  output(D, 4), // 0
+  output(B, 5), // 1
+  output(B, 4), // 2
+};
+
+static const output_t segment[] = {
+  output(D, 5), // A
+  output(A, 2), // B
+  output(C, 7), // C
+  output(D, 3), // D
+  output(D, 1), // E
+  output(A, 1), // F
+  output(C, 6), // G
+  output(D, 2), // P
+};
+
+static const uint8_t segment_map[] = {
+  /* ' ' */ seg_none,
+  /* '!' */ seg_none,
+  /* '"' */ seg_none,
+  /* '#' */ seg_none,
+  /* '$' */ seg_none,
+  /* '%' */ seg_none,
+  /* '&' */ seg_none,
+  /* ''' */ seg_none,
+  /* '(' */ seg_a | seg_d | seg_e | seg_f,
+  /* ')' */ seg_a | seg_b | seg_c | seg_d,
+  /* '*' */ seg_none,
+  /* '+' */ seg_none,
+  /* ',' */ seg_none,
+  /* '-' */ seg_none,
+  /* '.' */ seg_none,
+  /* '/' */ seg_none,
+  /* '0' */ seg_a | seg_b | seg_c | seg_d | seg_e | seg_f,
+  /* '1' */ seg_b | seg_c,
+  /* '2' */ seg_a | seg_b | seg_d | seg_e | seg_g,
+  /* '3' */ seg_a | seg_b | seg_c | seg_d | seg_g,
+  /* '4' */ seg_b | seg_c | seg_f | seg_g,
+  /* '5' */ seg_a | seg_c | seg_d | seg_f | seg_g,
+  /* '6' */ seg_a | seg_c | seg_d | seg_e | seg_f | seg_g,
+  /* '7' */ seg_a | seg_b | seg_c,
+  /* '8' */ seg_a | seg_b | seg_c | seg_d | seg_e | seg_f | seg_g,
+  /* '9' */ seg_a | seg_b | seg_c | seg_d | seg_f | seg_g,
+  /* ':' */ seg_none,
+  /* ';' */ seg_none,
+  /* '<' */ seg_none,
+  /* '=' */ seg_none,
+  /* '>' */ seg_none,
+  /* '?' */ seg_none,
+  /* '@' */ seg_none,
+  /* 'A' */ seg_a | seg_b | seg_c | seg_e | seg_f | seg_g,
+  /* 'B' */ seg_c | seg_d | seg_e | seg_f | seg_g,
+  /* 'C' */ seg_a | seg_d | seg_e | seg_f,
+  /* 'D' */ seg_b | seg_c | seg_d | seg_e | seg_g,
+  /* 'E' */ seg_a | seg_d | seg_e | seg_f | seg_g,
+  /* 'F' */ seg_a | seg_e | seg_f | seg_g,
+  /* 'G' */ seg_a | seg_b | seg_c | seg_d | seg_f | seg_g,
+  /* 'H' */ seg_b | seg_c | seg_e | seg_f | seg_g,
+  /* 'I' */ seg_b | seg_c,
+  /* 'J' */ seg_b | seg_c | seg_d | seg_e,
+  /* 'K' */ seg_d | seg_e | seg_f | seg_g,
+  /* 'L' */ seg_d | seg_e | seg_f,
+  /* 'M' */ seg_c | seg_e | seg_g,
+  /* 'N' */ seg_c | seg_e | seg_g,
+  /* 'O' */ seg_a | seg_b | seg_c | seg_d | seg_e | seg_f,
+  /* 'P' */ seg_a | seg_b | seg_e | seg_f | seg_g,
+  /* 'Q' */ seg_a | seg_b | seg_c | seg_f | seg_g,
+  /* 'R' */ seg_e | seg_g,
+  /* 'S' */ seg_a | seg_c | seg_d | seg_f | seg_g,
+  /* 'T' */ seg_d | seg_e | seg_f | seg_g,
+  /* 'U' */ seg_b | seg_c | seg_d | seg_e | seg_f,
+  /* 'V' */ seg_c | seg_d | seg_e,
+  /* 'W' */ seg_c | seg_d | seg_e,
+  /* 'X' */ seg_b | seg_c | seg_e | seg_f | seg_g,
+  /* 'Y' */ seg_b | seg_c | seg_d | seg_f | seg_g,
+  /* 'Z' */ seg_a | seg_b | seg_d | seg_e | seg_g,
+};
 
 static tiny_event_subscription_t interrupt_subscription;
 static uint8_t current_digit;
 static char display_string_buffer[3];
 static char display_string[3];
 
-static void digit_on(int section) {
-  switch(section) {
-    case 0:
-      write_output(D, 4, 0);
-      break;
+static void init_output(const output_t* output) {
+  output->port->DDR |= output->mask;
+  output->port->CR1 |= output->mask;
+}
 
-    case 1:
-      write_output(B, 5, 0);
-      break;
-
-    case 2:
-      write_output(B, 4, 0);
-      break;
-
-    default:
-      break;
+static void write_output(const output_t* output, uint8_t state) {
+  if(state) {
+    output->port->ODR |= output->mask;
+  }
+  else {
+    output->port->ODR &= ~output->mask;
   }
 }
 
-static void digit_off(int section) {
-  switch(section) {
-    case 0:
-      write_output(D, 4, 1);
-      break;
+static void display_character(char c) {
+  uint8_t index = c - ' ';
 
-    case 1:
-      write_output(B, 5, 1);
-      break;
-
-    case 2:
-      write_output(B, 4, 1);
-      break;
-
-    default:
-      break;
+  for(uint8_t i = 0; i < element_count(segment); i++) {
+    write_output(&segment[i], segment_map[index] & (1 << i));
   }
 }
 
-static void display_character(char character) {
-  switch(character) {
-    case 0:
-    case ' ':
-      write_output(D, 5, 0); // A
-      write_output(A, 2, 0); // B
-      write_output(C, 7, 0); // C
-      write_output(D, 3, 0); // D
-      write_output(D, 1, 0); // E
-      write_output(A, 1, 0); // F
-      write_output(C, 6, 0); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '0':
-      write_output(D, 5, 1); // A
-      write_output(A, 2, 1); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 1); // D
-      write_output(D, 1, 1); // E
-      write_output(A, 1, 1); // F
-      write_output(C, 6, 0); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '1':
-      write_output(D, 5, 0); // A
-      write_output(A, 2, 1); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 0); // D
-      write_output(D, 1, 0); // E
-      write_output(A, 1, 0); // F
-      write_output(C, 6, 0); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '2':
-      write_output(D, 5, 1); // A
-      write_output(A, 2, 1); // B
-      write_output(C, 7, 0); // C
-      write_output(D, 3, 1); // D
-      write_output(D, 1, 1); // E
-      write_output(A, 1, 0); // F
-      write_output(C, 6, 1); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '3':
-      write_output(D, 5, 1); // A
-      write_output(A, 2, 1); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 1); // D
-      write_output(D, 1, 0); // E
-      write_output(A, 1, 0); // F
-      write_output(C, 6, 1); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '4':
-      write_output(D, 5, 0); // A
-      write_output(A, 2, 1); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 0); // D
-      write_output(D, 1, 0); // E
-      write_output(A, 1, 1); // F
-      write_output(C, 6, 1); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '5':
-      write_output(D, 5, 1); // A
-      write_output(A, 2, 0); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 1); // D
-      write_output(D, 1, 0); // E
-      write_output(A, 1, 1); // F
-      write_output(C, 6, 1); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '6':
-      write_output(D, 5, 1); // A
-      write_output(A, 2, 0); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 1); // D
-      write_output(D, 1, 1); // E
-      write_output(A, 1, 1); // F
-      write_output(C, 6, 1); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '7':
-      write_output(D, 5, 1); // A
-      write_output(A, 2, 1); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 0); // D
-      write_output(D, 1, 0); // E
-      write_output(A, 1, 0); // F
-      write_output(C, 6, 0); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '8':
-      write_output(D, 5, 1); // A
-      write_output(A, 2, 1); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 1); // D
-      write_output(D, 1, 1); // E
-      write_output(A, 1, 1); // F
-      write_output(C, 6, 1); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    case '9':
-      write_output(D, 5, 1); // A
-      write_output(A, 2, 1); // B
-      write_output(C, 7, 1); // C
-      write_output(D, 3, 1); // D
-      write_output(D, 1, 0); // E
-      write_output(A, 1, 1); // F
-      write_output(C, 6, 1); // G
-      write_output(D, 2, 0); // P
-      break;
-
-    default:
-      break;
-  }
-}
-
-static void do_stuff(void* context, const void* args) {
+static void scan(void* context, const void* args) {
   (void)context;
   (void)args;
 
-  if(current_digit == 2) {
+  if(current_digit == 0) {
     display_string[0] = display_string_buffer[0];
     display_string[1] = display_string_buffer[1];
     display_string[2] = display_string_buffer[2];
   }
 
   display_character(' ');
-  digit_off(current_digit);
-  current_digit = (current_digit + 1) % 3;
+  write_output(&digit[current_digit], digit_off);
+  current_digit = (current_digit + 1) % element_count(digit);
   display_character(display_string[current_digit]);
-  digit_on(current_digit);
+  write_output(&digit[current_digit], digit_on);
 }
 
 void display_init(i_tiny_event_t* interrupt) {
-  // Turns everything on
-  init_output(D, 5); // A
-  init_output(A, 2); // B
-  init_output(C, 7); // C
-  init_output(D, 3); // D
-  init_output(D, 1); // E
-  init_output(A, 1); // F
-  init_output(C, 6); // G
-  init_output(D, 2); // P
+  for(uint8_t i = 0; i < element_count(digit); i++) {
+    init_output(&digit[i]);
+  }
 
-  init_output(D, 4); // 1
-  init_output(B, 5); // 2
-  init_output(B, 4); // 3
+  for(uint8_t i = 0; i < element_count(segment); i++) {
+    init_output(&segment[i]);
+  }
 
-  digit_off(0);
-  digit_off(1);
-  digit_off(2);
-
-  tiny_event_subscription_init(&interrupt_subscription, NULL, do_stuff);
+  tiny_event_subscription_init(&interrupt_subscription, NULL, scan);
   tiny_event_subscribe(interrupt, &interrupt_subscription);
 }
 
--- src/main.c
+++ src/main.c
@@ -12,30 +12,25 @@
 #include "pc5_heartbeat.h"
 #include "tiny_timer.h"
 #include "watchdog.h"
+#include "display.h"
 
 static tiny_timer_group_t timer_group;
-static tiny_timer_t timer;
-
-static void kick_watchdog(tiny_timer_group_t* timer_group, void* context) {
-  (void)context;
-  watchdog_kick();
-  tiny_timer_start(timer_group, &timer, 1, kick_watchdog, NULL);
-}
 
 void main(void) {
   disableInterrupts();
   {
     watchdog_init();
     clock_init();
-    // tiny_timer_group_init(&timer_group, tim4_system_tick_init());
-    // pa3_heartbeat_init(&timer_group);
+    tiny_timer_group_init(&timer_group, tim4_system_tick_init());
+    pa3_heartbeat_init(&timer_group);
+    display_init(tim4_system_tick_interrupt());
+    display_write("XYZ");
   }
   enableInterrupts();
 
-  // kick_watchdog(&timer_group, NULL);
-
   while(true) {
-    // tiny_timer_group_run(&timer_group);
+    tiny_timer_group_run(&timer_group);
+    watchdog_kick();
     wfi();
   }
 }
