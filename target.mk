2f94afd6ca74415dd34458b412169a9c9e5f07fd
Ryan Hartlage <ryanplusplus@gmail.com>
2022-04-23 14:48:37 -0400

    Updated tools, switched to 4.2.0

--- target.mk
+++ target.mk
@@ -1,3 +1,5 @@
+include tools/setup.mk
+
 TARGET := w1209
 BUILD_DIR := build
 
@@ -6,7 +8,7 @@ DEVICE_TYPE := STM8S103
 STLINK := stlinkv2
 OPENOCD_CFG := tools/openocd/stm8s103.cfg
 SVD := tools/svd/stm8s103f3.svd
-TOOLCHAIN_VERSION := 4.0.0
+TOOLCHAIN_VERSION := 4.2.0
 
 include tools/defaults.mk
 
